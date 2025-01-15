6dfde61cd93dad56f9b6064d9d28f34a6381070c
ChoiYoungKyu <mpd253@naver.com>
2025-01-16 00:15:50 +0900

    🚨Fix: options API -> composition API

--- Loopin/src/components/header/LogoHeader.vue
+++ Loopin/src/components/header/LogoHeader.vue
@@ -1,17 +1,10 @@
-<script>
-export default {
-  name: "LogoHeader",
-};
-</script>
+<script setup></script>
 
 <template>
   <div class="h-[80px] bg-[#d9d9d9]">
     <header class="flex items-center w-full h-full ml-10">
       <h1 class="text-3xl">Loopin</h1>
     </header>
-    <main>
-      <slot />
-    </main>
   </div>
 </template>
 
