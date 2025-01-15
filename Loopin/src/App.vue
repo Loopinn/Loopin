c8f8204c71a002d40db477712c7dea3c8140eba5
ChoiYoungKyu <mpd253@naver.com>
2025-01-15 22:23:50 +0900

    ✨Feature: Header 컴포넌트 적용

--- Loopin/src/App.vue
+++ Loopin/src/App.vue
@@ -1,17 +1,32 @@
 <script setup>
 import { RouterLink } from "vue-router";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+import LogoHeader from "@/components/header/LogoHeader.vue";
+import CenteredHeader from "@/components/header/CenteredHeader.vue";
+
+const route = useRoute();
+
+const headerComponent = computed(() => {
+  const layoutName = route.meta.layout || "LogoHeader";
+  return layoutName === "CenteredHeader" ? CenteredHeader : LogoHeader;
+});
 </script>
 <template>
-  <nav class="flex gap-3">
-    <RouterLink to="/">메인</RouterLink>
-    <RouterLink to="/socialing">소셜링</RouterLink>
-    <RouterLink to="/club">클럽</RouterLink>
-    <RouterLink to="/challenge">챌린지</RouterLink>
-    <RouterLink to="/lounge">라운지</RouterLink>
-    <RouterLink to="/signIn">로그인</RouterLink>
-    <RouterLink to="/signUp">회원가입</RouterLink>
-    <RouterLink to="/user">프로필</RouterLink>
-  </nav>
-  <RouterView />
+  <div>
+    <component :is="headerComponent" />
+    <nav class="flex gap-3">
+      <RouterLink to="/">메인</RouterLink>
+      <RouterLink to="/socialing">소셜링</RouterLink>
+      <RouterLink to="/club">클럽</RouterLink>
+      <RouterLink to="/challenge">챌린지</RouterLink>
+      <RouterLink to="/lounge">라운지</RouterLink>
+      <RouterLink to="/signIn">로그인</RouterLink>
+      <RouterLink to="/signUp">회원가입</RouterLink>
+      <RouterLink to="/user">프로필</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 <style scoped></style>
