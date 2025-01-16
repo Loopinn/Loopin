4be8f633983dd1b64a7759c6889b9d11e3d0b928
denshou <zecaumr@naver.com>
2025-01-16 12:32:33 +0900

    [merge / JHS] ✨Feature: posts 이미지 등록 시 storage와 연결

--- Loopin/src/views/MainView.vue
+++ Loopin/src/views/MainView.vue
@@ -1,8 +1,5 @@
-<script setup>
-import GetPosts from "@/components/GetPosts.vue";
-</script>
+<script setup></script>
 <template>
   <h1>메인 입니다.</h1>
-  <GetPosts />
 </template>
 <style scoped></style>
