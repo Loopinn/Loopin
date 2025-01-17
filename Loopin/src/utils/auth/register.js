ae5ea68df15350273bdd2fb280f6a87d2090fbcb
denshou <zecaumr@naver.com>
2025-01-17 12:42:18 +0900

    [merge / JHS] ✨ FEAT: 유저 정보를 userinfo 테이블에서 가져오도록 수정. 유저 정보 수정 시 실시간으로 업데이트 되도록 수정

--- Loopin/src/utils/auth/register.js
+++ Loopin/src/utils/auth/register.js
@@ -6,10 +6,11 @@ export const register = async (uemail, upassword, unickname) => {
       email: uemail,
       password: upassword,
       options: {
-        data: { nickname: unickname, introduction: "안녕하세요.", profileImage: null },
+        data: { nickname: unickname, description: "안녕하세요.", profile_img: null },
       },
     });
     console.log(response);
+
     return response;
   } catch (error) {
     console.error(error);
