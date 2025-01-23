05a1a9553a975401013db086ee24aa38f5f82c52
ChoiYoungKyu <mpd253@naver.com>
2025-01-23 16:23:30 +0900

    💄Style: Header 색상 수정

--- Loopin/src/components/header/LogoHeader.vue
+++ Loopin/src/components/header/LogoHeader.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
-  <div class="h-[80px] bg-[#d9d9d9]">
-    <header class="flex items-center w-full h-full ml-10">
+  <div class="h-[80px] bg-white">
+    <header class="flex items-center w-full h-full ml-4">
       <h1 class="text-3xl">Loopin</h1>
     </header>
   </div>
