2e38c8abce7534a220853510872e5f694c28142e
kkyuheak <gyuheak@gmail.com>
2025-01-31 18:29:05 +0900

    Fix 유저페이지 참가한 모임 없음 구현

--- Loopin/src/components/userinfo/UserInfoFeed.vue
+++ Loopin/src/components/userinfo/UserInfoFeed.vue
@@ -7,7 +7,7 @@ const router = useRouter();
 </script>
 <template>
   <img
-    :src="props.feedData.images[0]"
+    :src="props.feedData?.images[0]"
     alt="피드 대표 이미지"
     class="w-[200px] h-[200px] bg-white border cursor-pointer object-cover"
     @click="router.push(`/lounge/${props.feedData.id}`)"
